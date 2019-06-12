2ecbe2376893305b6ab951b0e52a9abbdd854c1d
Chad Purdy <chpurdy@gmail.com>
2019-06-11 20:22:56 -0500

    lettter

---
+++ units/unit 1/week 1/letter.py
@@ -0,0 +1,20 @@
+
+to_name = "Student"
+from_name = "Teacher"
+
+
+print('-'*79)
+print(f"Dear {to_name},")
+print()
+print('Lorem Ipsum is simply dummy text of the printing and typesetting industry.') 
+print("Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,")
+print("when an unknown printer took a galley of type and scrambled it to make a type")
+print("specimen book. It has survived not only five centuries, but also the leap into")
+print("electronic typesetting, remaining essentially unchanged. It was popularised in")
+print("the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,")
+print("and more recently with desktop publishing software like Aldus PageMaker")
+print("including versions of Lorem Ipsum.")
+print()
+print("Sincerely,")
+print(f"{from_name}")
+print('-'*79)
